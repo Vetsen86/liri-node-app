de0785373adc2a5d5aa13f5a98091eb2a0d2ce8d
Erik Beckstrand <erik.beckstrand@gmail.com>
2019-11-05 16:49:56 -0700

    command logic added

--- liri.js
+++ liri.js
@@ -3,3 +3,27 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+
+var command = process.argv[2];
+
+switch (command) {
+    case "concert-this":
+        findConcert();
+        break;
+    case "spotify-this-song":
+        break;
+    case "movie-this":
+        break;
+    case "do-what-it-says":
+        break;
+    default:
+        console.log("Please enter a valid command.");
+        break;
+}
+
+function findConcert() {
+    var artist = process.argv[3];
+    
+}
